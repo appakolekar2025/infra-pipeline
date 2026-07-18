439f369891741770925b99d068d4024f2e85b403
appakolekar2025 <appakolekar2025@gmail.com>
2026-07-18 13:06:38 +0530

    adding code

--- main.tf
+++ main.tf
@@ -1,26 +1,16 @@
 terraform {
-    
   required_providers {
-    azurerm={
-        source = "hashicorp/azurerm"
+    azurerm = {
+      source = "hashicorp/azurerm"
     }
   }
-  backend "azurerm" {
-    resource_group_name = "production-rg"
-    storage_account_name = "prodstorage1998"
-    container_name = "container"
-    key = "terraform-tf-state"
-    
-  }
-  
-}
-provider "azurerm" {
-    features {
-      
-    }
-  
+
+  backend "azurerm" {}
 }
 
+provider "azurerm" {
+  features {}
+}
 resource "azurerm_resource_group" "RG" {
   name = "prod-rg"
   location = "centralindia"

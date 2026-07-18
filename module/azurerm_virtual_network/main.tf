457bab31de6652ab3a40f22df99389f31620fc2c
appakolekar2025 <appakolekar2025@gmail.com>
2026-07-19 01:56:18 +0530

    adding code

--- module/azurerm_virtual_network/main.tf
+++ module/azurerm_virtual_network/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network" "vnet" {
     for_each = var.vnets
-  name = var.vnets
+  name = each.value.name
   resource_group_name = each.value.resource_group_name
   location = each.value.location
   address_space = each.value.address_space
